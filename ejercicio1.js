8956a1e5e61a651eed422af16459c8070c618f7e
annahico <annahico@gmail.com>
2024-11-11 11:18:46 +0100

    first commit

---
+++ ejercicio1.js
@@ -0,0 +1,9 @@
+// Dada una cadena de TextDecoder, comprobar si e sun palíndromo o Node. No usar funciones de JS, solo estrucutras de control 
+
+// Los palíndromos son palabras que se leen igual aún estando invertidas
+// Por ejemplo: AnalyserNode, bob, otto, allivessevilla
+
+// Ejemplos:
+// esPalindromo("otto") // devuelve: True
+// esPalindromo("victor") // devuelve: False
+
